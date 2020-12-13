3e04ee25e56e052e6e408346af6f83da58ab2b68
aftabur <parvez.feits@gmail.com>
2020-12-14 01:02:51 +0600

    o

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,73 @@
-jQuery("#partner-slider").owlCarousel({
-    autoplay: true,
-    lazyLoad: true,
-    loop: false,
-    animateOut: 'fadeOut',
-    margin: 20,
-    responsiveClass: true,
-    autoHeight: false,
-    autoplayTimeout: 7000,
-    smartSpeed: 800,
-    dots: false,
-    nav:false,
-    navigation : true,
-    responsive: {
-      0: {
-        items: 3,
+// jQuery("#partner-slider").owlCarousel({
+//     autoplay: true,
+//     lazyLoad: true,
+//     loop: false,
+//     animateOut: 'fadeOut',
+//     margin: 20,
+//     responsiveClass: true,
+//     autoHeight: false,
+//     autoplayTimeout: 7000,
+//     smartSpeed: 800,
+//     dots: false,
+//     nav:false,
+//     navigation : true,
+//     responsive: {
+//       0: {
+//         items: 3,
  
-      },
+//       },
   
-      600: {
-        items: 4,
+//       600: {
+//         items: 4,
         
-      },
+//       },
   
-      1024: {
-        items: 6,
+//       1024: {
+//         items: 6,
         
-      },
+//       },
   
-      1366: {
-        items: 6,   
+//       1366: {
+//         items: 6,   
 	      
-      }
+//       }
+//     },
+// });
+
+jQuery("#partner-slider-main").owlCarousel({
+  autoplay: true,
+  lazyLoad: true,
+  loop: false,
+  animateOut: 'fadeOut',
+  margin: 20,
+  responsiveClass: true,
+  autoHeight: false,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  dots: false,
+  nav:false,
+  navigation : true,
+  responsive: {
+    0: {
+      items: 2,
+
     },
+
+    600: {
+      items: 4,
+      
+    },
+
+    1024: {
+      items: 6,
+      
+    },
+
+    1366: {
+      items: 6,   
+      
+    }
+  },
 });
 
 jQuery("#recent-client-slider").owlCarousel({
@@ -68,4 +104,40 @@ jQuery("#recent-client-slider").owlCarousel({
 	      
       }
     },
+});
+
+jQuery("#blog-slider").owlCarousel({
+  autoplay: true,
+  lazyLoad: true,
+  loop: true,
+  animateOut: 'fadeOut',
+  margin: 20,
+  responsiveClass: true,
+  autoHeight: false,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  dots: false,
+  nav:false,
+  navigation : true,
+  responsive: {
+    0: {
+      items: 1,
+
+    },
+
+    600: {
+      items: 2,
+      
+    },
+
+    1024: {
+      items: 3,
+      
+    },
+
+    1366: {
+      items:3,   
+      
+    }
+  },
 });
